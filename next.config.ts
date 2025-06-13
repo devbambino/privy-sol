404009234058db0315262e424bab0b7f6c5ec552
michaelessiet <emsaa2002@gmail.com>
2025-06-13 16:23:25 +0100

    ...

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,6 @@ const nextConfig: NextConfig = {
 =======
   /* config options here */
   webpack: (config) => {
-    // ...
     config.externals["@solana/web3.js"] = "commonjs @solana/web3.js";
     config.externals["@solana/spl-token"] = "commonjs @solana/spl-token";
     return config;
