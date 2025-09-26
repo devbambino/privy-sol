4568516d447f9c061d0776a1a66ca5c9031a4881
Dev Bambino <devbambinoacc@gmail.com>
2025-09-26 21:47:55 +0000

    issues not solved

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   webpack: (config) => {
+    config.externals['@solana/kit'] = 'commonjs @solana/kit';
+    config.externals['@solana-program/memo'] = 'commonjs @solana-program/memo';
+    config.externals['@solana-program/system'] = 'commonjs @solana-program/system';
+    config.externals['@solana-program/token'] = 'commonjs @solana-program/token';
     config.externals["@solana/web3.js"] = "commonjs @solana/web3.js";
     config.externals["@solana/spl-token"] = "commonjs @solana/spl-token";
     return config;
